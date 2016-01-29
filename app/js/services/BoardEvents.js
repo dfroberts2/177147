9fc81be8f930828c10ffbec2ab5b9c8eefd64a17
David Roberts <dfroberts2@gmail.com>
2016-01-29 00:24:31 -0500

    Create generate board number function

--- app/js/services/BoardEvents.js
+++ app/js/services/BoardEvents.js
@@ -1,13 +1,15 @@
 threesApp.factory('boardEvents',function(){
 	return {
 		generateNumber: function(board) {
-			var random = Math.floor(Math.random() * 16)
-			while (!board.flattenBoard[random]) {
-				var col = random % 4
-				var row = Math.floor(random / 4	)
-				board.values[row][col] = 3
-				random = Math.floor(Math.random() * 16)
-				break;
+			var random = Math.floor(Math.random() * 16);
+			if (!board.flattenBoard()[random]) {
+				var col = random % 4;
+				var row = Math.floor(random / 4	);
+				board.values[row][col] = 3;
+			}else if (!_.contains(board.flattenBoard(), null)) {
+				console.log('Gameover')
+			} else {
+				this.generateNumber(board);
 			}
 		}
 	}
