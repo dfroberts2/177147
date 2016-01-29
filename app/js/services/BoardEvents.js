a7484b1d1ef5481ebdbd720360448d80bd2953e7
David Roberts <dfroberts2@gmail.com>
2016-01-28 23:56:29 -0500

    Begin generate number logic

--- app/js/services/BoardEvents.js
+++ app/js/services/BoardEvents.js
@@ -1,7 +1,14 @@
 threesApp.factory('boardEvents',function(){
 	return {
 		generateNumber: function(board) {
-			console.log(board)
+			var random = Math.floor(Math.random() * 16)
+			while (!board.flattenBoard[random]) {
+				var col = random % 4
+				var row = Math.floor(random / 4	)
+				board.values[row][col] = 3
+				random = Math.floor(Math.random() * 16)
+				break;
+			}
 		}
 	}
 });
